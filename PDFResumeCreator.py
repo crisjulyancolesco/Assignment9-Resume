adb2888dd270836069a68f89f76d6af4df2f7b6d
cirsjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-02-10 16:17:54 +0800

    First part working properly

--- PDFResumeCreator.py
+++ PDFResumeCreator.py
@@ -15,14 +15,44 @@ json_file = open("Details.json", "r+")
 Resume = json.load(json_file)
 
 Name = Resume["Name"]
+Address = Resume["Address"]
+Contact = Resume["Contact No."]
+Email = Resume["Email Address"]
+Title1 = Resume["Title1"]
+Title2 = Resume["Title2"]
+Course = Resume["Course"]
+School = Resume["School"]
+Year = Resume["Year"]
+Title3 = Resume["Title3"]
+Skills = Resume["Skills"]
+Title4 = Resume["Title4"]
+Work1 = Resume["Work1"]
+Date1 = Resume["Date1"]
+Position1 = Resume["Position1"]
+Work2 = Resume["Work2"]
+Date2 = Resume["Date2"]
+Position2 = Resume["Position2"]
+Work3 = Resume["Work3"]
+Date3 = Resume["Date3"]
+Position3 = Resume["Position3"]
+Title5 = Resume["Title5"]
+Ref = Resume["Ref"]
 
-
-Pdf = FPDF()
+Pdf = FPDF("P", "mm", "Legal")
 Pdf.add_page()
 
-Pdf.set_font("Arial")
-Pdf.set_font_size(12)
+Pdf.set_font("Arial", "B", 15)
+Pdf.cell(0,0, Name, align = "L", ln=True)
 
-Pdf.cell(200, 10, txt = Name, ln = 3, align ='C')
+Pdf.set_font("Arial","", 12)
+Pdf.cell(40,10, Address, align = "L", ln=1)
+Pdf.cell(40,10, Contact, align = "L",ln=1)
+Pdf.cell(40,10, Email, align = "L", ln=1)
+
+
+
+
+
+
 
 Pdf.output("OLESCO_CRISJULYAN.pdf")
