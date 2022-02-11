46af7178d83c8403d62da7c08dff0d9690e7bcef
cirsjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-02-11 14:32:42 +0800

    Adding more details and lines

--- PDFResumeCreator.py
+++ PDFResumeCreator.py
@@ -29,15 +29,14 @@ Skill2 = Resume["Skill2"]
 Skill3 = Resume["Skill3"]
 Title4 = Resume["Title4"]
 Work1 = Resume["Work1"]
-Date1 = Resume["Date1"]
-Position1 = Resume["Position1"]
 Work2 = Resume["Work2"]
-Date2 = Resume["Date2"]
-Position2 = Resume["Position2"]
 Work3 = Resume["Work3"]
-Date3 = Resume["Date3"]
-Position3 = Resume["Position3"]
 Title5 = Resume["Title5"]
+Project1 = Resume["Project1"]
+Desc1 = Resume["Desc1"]
+Project2 = Resume["Project2"]
+Desc2 = Resume["Desc2"]
+Title6 = Resume["Title6"]
 Ref = Resume["Ref"]
 
 # PDF
@@ -45,64 +44,77 @@ Pdf = FPDF("P", "cm", "Letter")
 Pdf.add_page()
 
 #Spaces
-Pdf.set_font("Arial", "B", 15)
-Pdf.cell(0,0.5, align = "L", ln=1)
-Pdf.cell(0,0.5, align = "L", ln=1)
-Pdf.cell(0,0.5, align = "L", ln=1)
+Pdf.set_font("Arial", "", 15)
+Pdf.cell(0,0.4, align = "L", ln=1)
 
 #Personal info
-Pdf.set_font("Arial", "B", 15)
+Pdf.set_font("Times", "B", 18)
 Pdf.cell(0,1, Name, align = "L", ln=True)
-Pdf.set_font("Arial","", 13)
+
+Pdf.set_font("Times","", 15)
 Pdf.cell(0,0.5, Address, align = "L", ln=1)
-Pdf.cell(0,0.5, Contact, align = "L",ln=1)
+Pdf.cell(0,1, Contact, align = "L",ln=1)
 Pdf.cell(0,0.5, Email, align = "L", ln=1)
 
 #Picture
-Pdf.image( "144x144px.jpg", 15, 1, 5 )
+Pdf.image( "144x144px.jpg", 15, 0.5, 6 )
 
 # Objective
-Pdf.set_font("Arial", "BU", 13)
-Pdf.cell(0,1.5, Title1, align = "L", ln=1)
+Pdf.set_font("Times", "BU", 15)
+Pdf.cell(0,2, Title1, align = "L", ln=1)
+
 
 #Education
-Pdf.set_font("Arial", "BU", 13)
-Pdf.cell(0,1.5, Title2, align = "L", ln=1)
-Pdf.set_font("Arial", "", 13)
+Pdf.set_font("Times", "BU", 15)
+Pdf.cell(0,2, Title2, align = "L", ln=1)
+
+Pdf.set_font("Times", "", 15)
 Pdf.cell(0,0.5, Course, align = "L", ln=2)
-Pdf.cell(0,0.5, School, align = "l",ln=True)
+Pdf.cell(0,1, School, align = "l",ln=True)
 Pdf.cell(0,0.5, Year, align = "L", ln=True)
 
 #Skills
-Pdf.set_font("Arial", "BU", 13)
-Pdf.cell(0,1.5, Title3, align = "L", ln=1)
-Pdf.set_font("Arial", "", 13)
+Pdf.set_font("Times", "BU", 15)
+Pdf.cell(0,2, Title3, align = "L", ln=1)
+
+Pdf.set_font("Times", "", 15)
 Pdf.cell(0,0.5, Skill1, align = "L", ln=1)
-Pdf.cell(0,0.5, Skill2, align = "L", ln=1)
+Pdf.cell(0,1, Skill2, align = "L", ln=1)
 Pdf.cell(0,0.5, Skill3, align = "L", ln=1)
 
 #Working Experience
-Pdf.set_font("Arial", "BU", 13)
-Pdf.cell(0,1.5, Title4, align = "L", ln=1)
+Pdf.set_font("Times", "BU", 15)
+Pdf.cell(0,2, Title4, align = "L", ln=1)
 
-Pdf.set_font("Arial", "", 13)
+Pdf.set_font("Times", "", 15)
 Pdf.cell(0,0.5, Work1, align = "L", ln=1)
 
-Pdf.set_font("Arial", "", 13)
-Pdf.cell(0,0.5, Work2, align = "L", ln=2)
+Pdf.set_font("Times", "", 15)
+Pdf.cell(0,1, Work2, align = "L", ln=2)
 
-Pdf.set_font("Arial", "", 13)
+Pdf.set_font("Times", "", 15)
 Pdf.cell(0,0.5, Work3, align = "L", ln=3)
 
+#Projects
+Pdf.set_font("Times", "BU", 15)
+Pdf.cell(0,2, Title5, align = "L", ln=1)
+
+Pdf.set_font("Times", "BI", 15)
+Pdf.cell(0,0.5, Project1, align = "L", ln=3)
+Pdf.set_font("Times", "", 15)
+Pdf.cell(0,1, Desc1, align = "L", ln=3)
+
+Pdf.set_font("Times", "BI", 15)
+Pdf.cell(0,1, Project2, align = "L", ln=3)
+Pdf.set_font("Times", "", 15)
+Pdf.cell(0,0.5, Desc2, align = "L", ln=3)
 
 #Char Ref
-Pdf.set_font("Arial", "BU", 13)
-Pdf.cell(0,1.5, Title5, align = "L", ln=1)
-Pdf.set_font("Arial", "", 13)
+Pdf.set_font("Times", "BU", 15)
+Pdf.cell(0,2, Title6, align = "L", ln=1)
+
+Pdf.set_font("Times", "", 15)
 Pdf.cell(0,0.5, Ref, align = "L", ln=1)
-
-
-
 
 
 Pdf.output("OLESCO_CRISJULYAN.pdf")
